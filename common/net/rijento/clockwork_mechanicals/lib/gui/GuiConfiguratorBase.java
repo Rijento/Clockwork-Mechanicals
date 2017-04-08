59407d3313ba71351621659cf6c86edbc5d6512a
Rijento <rijento@gmail.com>
2017-04-08 09:14:55 -0400

    added some text and black/whitelist buttons on pickup and dropoff to gui.

--- common/net/rijento/clockwork_mechanicals/lib/gui/GuiConfiguratorBase.java
+++ common/net/rijento/clockwork_mechanicals/lib/gui/GuiConfiguratorBase.java
@@ -1,5 +1,7 @@
 package net.rijento.clockwork_mechanicals.lib.gui;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -37,6 +39,8 @@ public class GuiConfiguratorBase extends GuiContainer
 	private Button buttonDeposit;
 	private Button buttonCraft;
 	private Button buttonWind;
+	private ButtonBW buttonBlackWhite1;
+	private ButtonBW buttonBlackWhite2;
 	public GuiConfiguratorBase(InventoryPlayer inventory, ItemStack configuratorIn, World worldIn)
 	{
 		super(new ContainerConfigurator(inventory, configuratorIn));
@@ -58,6 +62,18 @@ public class GuiConfiguratorBase extends GuiContainer
         buttonList.add(buttonMine = new Button(4,i+70,j+7));
         buttonList.add(buttonTake = new Button(5,i+88,j+7));
         buttonList.add(buttonDeposit = new Button(6,i+106,j+7));
+        
+        if (selected == 5)
+        {
+        	if (buttonBlackWhite1 == null){buttonList.add(buttonBlackWhite1 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43));}
+        	else{buttonList.add(buttonBlackWhite1);}
+        }
+        if (selected == 6)
+        {
+        	if (buttonBlackWhite2 == null){buttonList.add(buttonBlackWhite2 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43));}
+        	else{buttonList.add(buttonBlackWhite2);}
+        }
+        
         buttonList.add(buttonCraft = new Button(7,i+124,j+7));
         buttonList.add(buttonWind = new Button(8,i+142,j+7));
         
@@ -72,8 +88,28 @@ public class GuiConfiguratorBase extends GuiContainer
 		else if (selected == 2){this.xSize = 176; this.ySize = 136; this.mc.getTextureManager().bindTexture(chopTexture);}
 		else if (selected == 3){this.xSize = 176; this.ySize = 136; this.mc.getTextureManager().bindTexture(slayTexture);}
 		else if (selected == 4){this.xSize = 176; this.ySize = 222; this.mc.getTextureManager().bindTexture(mineTexture);}
-		else if (selected == 5){this.xSize = 176; this.ySize = 222; this.mc.getTextureManager().bindTexture(withdrawTexture);}
-		else if (selected == 6){this.xSize = 176; this.ySize = 222; this.mc.getTextureManager().bindTexture(depositTexture);}
+		else if (selected == 5)
+		{
+			this.xSize = 176;
+			this.ySize = 222;
+			this.mc.getTextureManager().bindTexture(withdrawTexture);
+			int i = (this.width - this.xSize) / 2;
+	        int j = (this.height - this.ySize) / 2;
+	        this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+			fontRendererObj.drawString("Filter", ((this.width - this.xSize) / 2) + 31, ((this.height - this.ySize) / 2) + 32, 1);
+			return;
+		}
+		else if (selected == 6)
+		{
+			this.xSize = 176;
+			this.ySize = 222;
+			this.mc.getTextureManager().bindTexture(depositTexture);
+			int i = (this.width - this.xSize) / 2;
+	        int j = (this.height - this.ySize) / 2;
+	        this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+			fontRendererObj.drawString("Filter", ((this.width - this.xSize) / 2) + 31, ((this.height - this.ySize) / 2) + 32, 1);
+			return;
+		}
 		else if (selected == 7){this.xSize = 176; this.ySize = 222; this.mc.getTextureManager().bindTexture(craftTexture);}
 		else if (selected == 8){this.xSize = 176; this.ySize = 136; this.mc.getTextureManager().bindTexture(windTexture);}
         int i = (this.width - this.xSize) / 2;
@@ -85,7 +121,6 @@ public class GuiConfiguratorBase extends GuiContainer
 	public void updateScreen()
     {
         super.updateScreen();
-        this.buttonList.clear();
         this.initGui();
     }
 	
@@ -132,6 +167,14 @@ public class GuiConfiguratorBase extends GuiContainer
 			((ContainerConfigurator)this.inventorySlots).current_task = 5;
 			selected = 5;
 			((ContainerConfigurator) this.inventorySlots).update();
+			if (buttonBlackWhite1 != null)
+			{
+				boolean temp = buttonBlackWhite1.BoW;
+				buttonBlackWhite1 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43);
+				buttonBlackWhite1.BoW = temp;	
+			}
+			else{buttonBlackWhite1 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43);}
+			
 		}
 		else if (parButton == buttonDeposit)
 		{
@@ -139,6 +182,13 @@ public class GuiConfiguratorBase extends GuiContainer
 			((ContainerConfigurator)this.inventorySlots).current_task = 6;
 			selected = 6;
 			((ContainerConfigurator) this.inventorySlots).update();
+			if (buttonBlackWhite2 != null)
+			{
+				boolean temp = buttonBlackWhite2.BoW;
+				buttonBlackWhite2 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43);
+				buttonBlackWhite2.BoW = temp;	
+			}
+			else{buttonBlackWhite2 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - 222) / 2) + 43);}
 		}
 		else if (parButton == buttonCraft)
 		{
@@ -153,6 +203,36 @@ public class GuiConfiguratorBase extends GuiContainer
 			((ContainerConfigurator)this.inventorySlots).current_task = 8;
 			selected = 8;
 			((ContainerConfigurator) this.inventorySlots).update();
+		}
+		else if (parButton == buttonBlackWhite1)
+		{
+			if (buttonBlackWhite1.BoW)
+			{
+				buttonBlackWhite1 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - this.ySize) / 2) + 43);
+				buttonBlackWhite1.BoW = false;
+				return;
+			}
+			else
+			{
+				buttonBlackWhite1 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - this.ySize) / 2) + 43);
+				buttonBlackWhite1.BoW = true;
+				return;
+			}
+		}
+		else if (parButton == buttonBlackWhite2)
+		{
+			if (buttonBlackWhite2.BoW)
+			{
+				buttonBlackWhite2 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - this.ySize) / 2) + 43);
+				buttonBlackWhite2.BoW = false;
+				return;
+			}
+			else
+			{
+				buttonBlackWhite2 = new ButtonBW(9, ((this.width - this.xSize) / 2) + 88, ((this.height - this.ySize) / 2) + 43);
+				buttonBlackWhite2.BoW = true;
+				return;
+			}
 		}
     }
 
@@ -169,7 +249,27 @@ public class GuiConfiguratorBase extends GuiContainer
         {
 			
         }
-		
 	}
-
+	@SideOnly (Side.CLIENT)
+	static class ButtonBW extends GuiButton
+	{
+		public boolean BoW = true;
+		public ButtonBW(int buttonId, int x, int y) {
+			super(buttonId, x, y, 18, 18, "");
+			// TODO Auto-generated constructor stub
+		}
+		@Override
+        public void drawButton(Minecraft mc, int parX, int parY)
+        {
+			if (visible)
+            {
+				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+                mc.getTextureManager().bindTexture(withdrawTexture);
+                int textureX = 176;
+                int textureY = 0;
+                if (! BoW){textureX += 18;}
+                drawTexturedModalRect(xPosition, yPosition, textureX, textureY, 18, 18);
+            }
+        }
+	}
 }
